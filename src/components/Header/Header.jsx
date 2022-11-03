96a3c75791b588e2b312e51e45999c2346722be4
unknown <vladislav.pikiner@gmail.com>
2022-11-03 16:48:18 +0200

    fix style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useTelegram } from '../hooks/useTelegram'
-
-
+import Button from '../Button/Button';
+import './Header.css'
 export default function Header() {
    const {user, onClose} = useTelegram();
   return (
     <>
-    <h1>👋 Welcome {user?.username} </h1>
-   <button onClick={onClose}>❌</button>
-   
+   <div className='header'>
+    <span className='username'>👋 Welcome {user?.username} </span>
+   <Button onClick={onClose}>❌</Button>
+   </div>
    </>
   )
 }
